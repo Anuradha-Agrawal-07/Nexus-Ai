dd21af01083ba63ab9909fd9ba0713b97a9ee8ba
Anuradha <paysur2111@gmail.com>
2026-07-07 12:36:42 +0530

    feat: implement complete project CRUD

--- backend/app/api/projects.py
+++ backend/app/api/projects.py
@@ -4,7 +4,7 @@ from fastapi import (
     HTTPException,
     status
 )
-
+from uuid import UUID
 from sqlalchemy.orm import Session
 
 from app.db.database import get_db
@@ -17,7 +17,11 @@ from app.schemas.project import (
 
 from app.crud.project import (
     get_project_by_name,
-    create_project
+    create_project,
+    get_projects_by_owner,
+    get_project_by_id,
+    update_project,
+    delete_project
 )
 
 from app.models.user import User
@@ -47,3 +51,69 @@ def create_new_project(
         )
     new_project=create_project(db,project,current_user)
     return new_project
+
+@router.get(
+    "/",
+    response_model=list[ProjectResponse]
+)
+def get_my_projects(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    projects=get_projects_by_owner(db,current_user.id)
+    return projects
+
+@router.get(
+    "/{project_id}",
+    response_model=ProjectResponse
+)
+def get_project(
+    project_id: UUID,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    project=get_project_by_id(db,project_id,current_user.id)
+    if not project:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project doesn't exist"
+        )
+    return project
+
+@router.patch(
+    "/{project_id}",
+    response_model=ProjectUpdate
+)
+def update_existing_project(
+    project_id:UUID,
+    project_update:ProjectUpdate,
+    db:Session=Depends(get_db), 
+    current_user:User=Depends(get_current_user)
+
+):
+    project=get_project_by_id(db,project_id,current_user.id)
+    if not project:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project doesn't exist"
+        )
+    updated_project=update_project(db,project,project_update)
+    return updated_project
+
+@router.delete(
+    "/{project_id}",
+    status_code=status.HTTP_204_NO_CONTENT
+)
+def delete_existing_project(
+    project_id:UUID,
+    db:Session=Depends(get_db), 
+    current_user:User=Depends(get_current_user)
+):
+    project=get_project_by_id(db,project_id,current_user.id)
+    if not project:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project doesn't exist"
+        )
+    delete_project(db,project)
+    return
--- backend/app/crud/project.py
+++ backend/app/crud/project.py
@@ -5,6 +5,7 @@ from app.models.user import User
 
 from app.schemas.project import (
     ProjectCreate,
+    ProjectResponse,
     ProjectUpdate
 )
 
@@ -27,3 +28,41 @@ def create_project(
     db.commit()
     db.refresh(new_project)
     return new_project
+
+def get_projects_by_owner(
+    db: Session,
+    owner_id: UUID
+):
+    return db.query(Project).filter(Project.owner_id==owner_id).all()
+
+def get_project_by_id(
+    db: Session,
+    project_id:UUID,
+    owner_id:UUID
+):
+    return db.query(Project).filter(Project.id==project_id,Project.owner_id==owner_id).first()
+
+def update_project(
+    db: Session ,
+    project: Project,
+    project_update:ProjectUpdate
+):
+    update_data = project_update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(project, key, value)
+        #If user sends  
+        # {
+        #     "name": "Nexus AI v2"
+        # }
+        #setattr makes it equivalent to project.name="Nexus AI v2"
+    db.commit()
+    db.refresh(project)
+    return project
+
+def delete_project(
+    db:Session,
+    project:Project
+):
+    db.delete(project)
+    db.commit()
+    return 
